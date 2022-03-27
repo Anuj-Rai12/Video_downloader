549ed846368cee199047775ea0dcc85e9a8d1dcf
Anuj <anujrai04082002.0@gmail.com>
2022-03-27 16:01:20 +0530

    Added the Dialog Box status:Successfully

--- app/src/main/java/com/example/videodownloadingline/ui/HomeScrFragment.kt
+++ app/src/main/java/com/example/videodownloadingline/ui/HomeScrFragment.kt
@@ -1,6 +1,6 @@
 package com.example.videodownloadingline.ui
 
-import android.content.ContentValues.TAG
+
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -11,7 +11,9 @@ import androidx.recyclerview.widget.GridLayoutManager
 import com.example.videodownloadingline.R
 import com.example.videodownloadingline.adaptor.iconadaptor.HomeSrcAdaptor
 import com.example.videodownloadingline.databinding.HomeSrcFragmentBinding
+import com.example.videodownloadingline.dialog.AddIconsDialogBox
 import com.example.videodownloadingline.model.homesrcicon.HomeSrcIcon
+import com.example.videodownloadingline.utils.TAG
 import com.example.videodownloadingline.utils.changeStatusBarColor
 import com.example.videodownloadingline.utils.hide
 import com.example.videodownloadingline.utils.hideFullSrc
@@ -21,12 +23,20 @@ class HomeScrFragment(private val searchClicked: () -> Unit) :
     Fragment(R.layout.home_src_fragment) {
     private lateinit var binding: HomeSrcFragmentBinding
     private lateinit var homeSrcAdaptor: HomeSrcAdaptor
+    private var iconsDialogBox: AddIconsDialogBox? = null
+    private var isDialogBoxIsVisible: Boolean = false
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = HomeSrcFragmentBinding.bind(view)
         initial()
+        savedInstanceState?.let {
+            isDialogBoxIsVisible = it.getBoolean(getString(R.string.add_to_home_src))
+        }
+        if (isDialogBoxIsVisible) {
+            showDialogBox()
+        }
         recycleAdaptor()
         setData()
         binding.srcTv.setOnClickListener {
@@ -35,11 +45,31 @@ class HomeScrFragment(private val searchClicked: () -> Unit) :
         }
     }
 
+    private fun showDialogBox() {
+        iconsDialogBox = AddIconsDialogBox()
+        iconsDialogBox?.show(context = requireActivity(), listenerForDismiss = {
+            iconsDialogBox?.dismiss()
+            isDialogBoxIsVisible = false
+        }, listenerIcon = {
+            Log.i(TAG, "showDialogBox: $it")
+            iconsDialogBox?.dismiss()
+        })
+        isDialogBoxIsVisible = true
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (isDialogBoxIsVisible) {
+            iconsDialogBox?.dismiss()
+        }
+    }
+
     private fun recycleAdaptor() {
         binding.homeSrcIcon.apply {
             layoutManager = GridLayoutManager(requireActivity(), 4)
             homeSrcAdaptor = HomeSrcAdaptor { data: HomeSrcIcon, isAddIcon: Boolean ->
                 Log.i(TAG, "recycleAdaptor: $isAddIcon with $data")
+                showDialogBox()
             }
             adapter = homeSrcAdaptor
         }
@@ -85,4 +115,10 @@ class HomeScrFragment(private val searchClicked: () -> Unit) :
         activity?.hideFullSrc()
         activity?.changeStatusBarColor()
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(getString(R.string.add_to_home_src), isDialogBoxIsVisible)
+    }
+
 }
