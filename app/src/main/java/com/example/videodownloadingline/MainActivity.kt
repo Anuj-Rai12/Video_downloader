fab833be1c9dd194f48cb71354aa17f7d635fe22
Anuj <anujrai04082002.0@gmail.com>
2022-04-01 09:46:46 +0530

    Changed the Color of download video progress status:Success

--- app/src/main/java/com/example/videodownloadingline/MainActivity.kt
+++ app/src/main/java/com/example/videodownloadingline/MainActivity.kt
@@ -2,17 +2,20 @@ package com.example.videodownloadingline
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.example.videodownloadingline.adaptor.viewpager_adaptor.ViewPagerAdaptor
 import com.example.videodownloadingline.databinding.ActivityMainBinding
+import com.example.videodownloadingline.utils.TAG
 import com.example.videodownloadingline.utils.hideActionBar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var currentScr: Int = 1
 
     companion object {
         var bottomNavigation: BottomNavigationView? = null
@@ -25,14 +28,17 @@ class MainActivity : AppCompatActivity() {
         this.hideActionBar()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        savedInstanceState?.let {
+            currentScr = it.getInt(getString(R.string.num_of_tab))
+            Log.i(TAG, "onCreate: Main Activity $currentScr")
+        }
         val adaptor = ViewPagerAdaptor(this)
         viewPager2 = binding.viewPager
         bottomNavigation = binding.bottomNavigation
 
         binding.viewPager.adapter = adaptor
         binding.viewPager.isUserInputEnabled = false
-        setCurrTab(1)
+        setCurrTab(currentScr)
         binding.bottomNavigation.selectedItemId = R.id.homeScrFragment
         bottomNav()
     }
@@ -64,12 +70,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     @SuppressLint("StringFormatMatches")
-    private fun setCurrTab(tab: Int? = null) {
-        tab?.let { binding.viewPager.currentItem = it }
+    private fun setCurrTab(tab: Int) {
+        binding.viewPager.currentItem = tab
+        currentScr = tab
     }
 
     override fun onPause() {
         super.onPause()
         binding.viewPager.adapter = null
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(getString(R.string.num_of_tab), currentScr)
     }
 }
