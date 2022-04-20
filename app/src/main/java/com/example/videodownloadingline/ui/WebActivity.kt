9263a1d08564d4ee5cb3047928e58d49dc13e54a
Anuj <anujrai04082002.0@gmail.com>
2022-04-20 10:41:01 +0530

    Added the ViewPager to Fragment status:Success

--- app/src/main/java/com/example/videodownloadingline/ui/WebActivity.kt
+++ app/src/main/java/com/example/videodownloadingline/ui/WebActivity.kt
@@ -7,52 +7,33 @@ import android.util.Log
 import android.view.Gravity
 import android.view.KeyEvent
 import android.view.Menu
-import android.view.View
-import android.webkit.WebChromeClient
-import android.webkit.WebSettings
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.view.menu.MenuBuilder
 import androidx.core.widget.doOnTextChanged
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.navArgs
 import com.example.videodownloadingline.R
+import com.example.videodownloadingline.adaptor.viewpager_adaptor.ViewPagerAdapter
 import com.example.videodownloadingline.databinding.ActivityWebBinding
 import com.example.videodownloadingline.databinding.CustomToolbarLayoutBinding
-import com.example.videodownloadingline.utils.*
-import com.example.videodownloadingline.view_model.MainViewModel
-import com.google.android.material.snackbar.Snackbar
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import com.example.videodownloadingline.utils.TAG
 import java.util.*
 
 class WebActivity : AppCompatActivity() {
     private val args: WebActivityArgs by navArgs()
     private lateinit var binding: ActivityWebBinding
-    private var goBack: Boolean = true
-    private var mainViewModel: MainViewModel? = null
+    private var viewPagerAdapter: ViewPagerAdapter? = null
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWebBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        mainViewModel = MainViewModel.getInstance()
-        if (applicationContext.isNetworkAvailable()) {
-            binding.mainWebView.show()
-            setWebSiteData()
-            listenForProgress()
-        } else {
-            binding.tapToDownloadIcon.visibility = View.INVISIBLE
-            binding.progressbar.isIndeterminate = true
-            binding.root.showSandbar(
-                "No Internet Connection Available", Snackbar.LENGTH_INDEFINITE,
-                getColorInt(R.color.Alizarin_Crimson_color)
-            )
-        }
+        viewPagerAdapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+        viewPagerAdapter?.setFragment(WebViewFragments(args.name, args.url))
+        binding.mainWebViewViewPager.adapter = viewPagerAdapter
+        binding.mainWebViewViewPager.isUserInputEnabled = false
     }
 
     @SuppressLint("RestrictedApi")
@@ -64,64 +45,10 @@ class WebActivity : AppCompatActivity() {
         return true
     }
 
-    @SuppressLint("SetJavaScriptEnabled")
-    private fun setWebSiteData() {
-        binding.mainWebView.apply {
-            webViewClient = WebViewClient()
-            loadUrl(args.url)
-        }
-        val webSettings: WebSettings = binding.mainWebView.settings
-        webSettings.javaScriptEnabled = true
-        webSettings.loadWithOverviewMode = true
-        webSettings.useWideViewPort = true
-        webSettings.domStorageEnabled = true
-        webSettings.loadsImagesAutomatically = true
-    }
-
-
-    private fun listenForProgress() {
-        binding.mainWebView.webChromeClient = object : WebChromeClient() {
-            override fun onProgressChanged(view: WebView?, newProgress: Int) {
-                Log.i(TAG, "onProgressChanged: $newProgress")
-                binding.progressbar.progress = newProgress
-                if (newProgress == 100) {
-                    binding.progressbar.progress = 0
-                    getAllTab()
-                    hideImage()
-                }
-                super.onProgressChanged(view, newProgress)
-            }
-        }
-    }
-
-    private fun getAllTab() {
-        mainViewModel?.noOfOpenTab?.observe(this) {
-            val item = it ?: 0
-            changeToolbar(item)
-        }
-    }
-
-    private fun hideImage() {
-        lifecycleScope.launch {
-            binding.tapToDownloadIcon.show()
-            delay(2000)
-            binding.tapToDownloadIcon.hide()
-        }
-    }
-
-    override fun onBackPressed() {
-        if (binding.mainWebView.canGoBack() && goBack) {
-            binding.mainWebView.goBack()
-        } else {
-            super.onBackPressed()
-        }
-    }
-
     override fun onResume() {
         super.onResume()
         supportActionBar!!.displayOptions = ActionBar.DISPLAY_SHOW_TITLE
         supportActionBar!!.setDisplayShowCustomEnabled(false)
-        supportActionBar!!.title = args.name
     }
 
     @SuppressLint("RtlHardcoded")
@@ -162,7 +89,6 @@ class WebActivity : AppCompatActivity() {
             true
         }
         toolbarBinding.toolbarHomeBtn.setOnClickListener {
-            goBack = false
             onBackPressed()
         }
         toolbarBinding.totalTabOp.text = String.format(
@@ -170,10 +96,5 @@ class WebActivity : AppCompatActivity() {
             "%d",
             totalTab
         )
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        binding.mainWebView.restoreState(savedInstanceState)
-        super.onRestoreInstanceState(savedInstanceState)
     }
 }
--- app/src/main/java/com/example/videodownloadingline/ui/WebViewFragments.kt
+++ app/src/main/java/com/example/videodownloadingline/ui/WebViewFragments.kt
@@ -1,26 +1,122 @@
 package com.example.videodownloadingline.ui
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.ActionBar
 import androidx.fragment.app.Fragment
-import com.example.videodownloadingline.MainActivity
+import androidx.lifecycle.lifecycleScope
 import com.example.videodownloadingline.R
 import com.example.videodownloadingline.databinding.WebSiteFragmentLayoutBinding
-import com.example.videodownloadingline.utils.show
+import com.example.videodownloadingline.utils.*
+import com.example.videodownloadingline.view_model.MainViewModel
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class WebViewFragments(private val search: String) : Fragment(R.layout.web_site_fragment_layout) {
+class WebViewFragments(private val title: String, private val url: String) :
+    Fragment(R.layout.web_site_fragment_layout) {
     private lateinit var binding: WebSiteFragmentLayoutBinding
+    private var mainViewModel: MainViewModel? = null
 
+    @RequiresApi(Build.VERSION_CODES.M)
     @SuppressLint("StringFormatMatches")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = WebSiteFragmentLayoutBinding.bind(view)
-        binding.toolBarMainActivity.searchBoxEd.show()
-        binding.toolBarMainActivity.searchBoxEd.setText(search)
-        binding.toolBarMainActivity.totalTabOp.text = getString(
-            R.string.num_of_tab,
-            MainActivity.viewPager2?.currentItem ?: 10
-        )
+
+        mainViewModel = MainViewModel.getInstance()
+        if (requireActivity().applicationContext.isNetworkAvailable()) {
+            binding.mainWebView.show()
+            setWebSiteData()
+            listenForProgress()
+        } else {
+            binding.tapToDownloadIcon.visibility = View.INVISIBLE
+            binding.progressbar.isIndeterminate = true
+            binding.root.showSandbar(
+                "No Internet Connection Available", Snackbar.LENGTH_INDEFINITE,
+                requireActivity().getColorInt(R.color.Alizarin_Crimson_color)
+            )
+        }
+        onBackPress()
     }
+
+    private fun onBackPress() {
+        activity?.onBackPressedDispatcher?.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    if (binding.mainWebView.canGoBack()) {
+                        binding.mainWebView.goBack()
+                    } else {
+                        isEnabled=false
+                        activity?.onBackPressed()
+                    }
+                }
+            })
+    }
+
+    private fun getAllTab() {
+        mainViewModel?.noOfOpenTab?.observe(this) {
+            val item = it ?: 0
+            (requireActivity() as WebActivity).changeToolbar(item)
+        }
+    }
+
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setWebSiteData() {
+        binding.mainWebView.apply {
+            webViewClient = WebViewClient()
+            loadUrl(this@WebViewFragments.url)
+        }
+        val webSettings: WebSettings = binding.mainWebView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.loadWithOverviewMode = true
+        webSettings.useWideViewPort = true
+        webSettings.domStorageEnabled = true
+        webSettings.loadsImagesAutomatically = true
+    }
+
+
+    private fun listenForProgress() {
+        binding.mainWebView.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                Log.i(TAG, "onProgressChanged: $newProgress")
+                binding.progressbar.progress = newProgress
+                if (newProgress == 100) {
+                    binding.progressbar.progress = 0
+                    getAllTab()
+                    hideImage()
+                }
+                super.onProgressChanged(view, newProgress)
+            }
+        }
+    }
+
+
+    private fun hideImage() {
+        lifecycleScope.launch {
+            binding.tapToDownloadIcon.show()
+            delay(2000)
+            binding.tapToDownloadIcon.hide()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (requireActivity() as WebActivity).supportActionBar!!.displayOptions =
+            ActionBar.DISPLAY_SHOW_TITLE
+        (requireActivity() as WebActivity).supportActionBar!!.setDisplayShowCustomEnabled(false)
+        (requireActivity() as WebActivity).supportActionBar!!.title = title
+    }
+
 }
